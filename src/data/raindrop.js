76be795baf34aa3220961fe21d2fe9028306f8ca
Shane Rogers <swrogers@gmail.com>
2024-04-03 13:11:38 -0500

    Raindrop.io Additions

---
+++ src/data/raindrop.js
@@ -0,0 +1,25 @@
+const axios = require("axios").default;
+const config = require("dotenv").config();
+
+module.exports = async function () {
+  let url = "https://api.raindrop.io/rest/v1/raindrops/0";
+
+  //    console.log("Token: ", `${process.env.RAINDROP_IO_KEY}`);
+
+  let res = await axios
+    .get(url, {
+      headers: { Authorization: `${process.env.RAINDROP_IO_KEY}` },
+    })
+    .catch((error) => {
+      console.log("Unable to fetch from raindrop.io: ", error);
+    });
+
+  let links = res.data.items;
+  /* links should have the following shape:
+   * _id, link, title, excerpt, note, type,
+   * cover (https link), tags, created (iso date),
+   * and other stuff
+   */
+
+  return links;
+};
